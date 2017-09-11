85e79d9ba32b280ca6623aaa0625ed9ff56c4776
jannlorenz <jannlorenzcchua@gmail.com>
2017-09-11 09:36:52 +0000

    hello world

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class Index(View):
+    template_name = 'notes/index.html'
+    
+    def get_context_data(self):
+        context = {}
+        return context
+        
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        return render(request, self.template_name, context)
