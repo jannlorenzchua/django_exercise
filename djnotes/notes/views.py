9ce14207944211fb870e6593355d145b618ce5b8
jannlorenz <jannlorenzcchua@gmail.com>
2017-09-14 03:23:23 +0000

    forms

--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.generic import View
 
 from .models import Note
+from .forms import NoteForm
 # Create your views here.
 
 class Index(View):
@@ -19,3 +20,11 @@ class Index(View):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data()
         return render(request, self.template_name, context)
+        
+    def post(self, request, *args, **kwargs):
+        form = NoteForm(request.POST)
+        context = self.get_context_data()
+        if form.is_valid():
+            form.save()
+        return render(request, self.template_name, context)
+        
